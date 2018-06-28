7651681263308321468942f1276e89dcf08b8fd8
Andrew Linxie <andrew.linxie@databricks.com>
2018-06-28 14:04:31 -0700

    python3 and 2 capability iwth datetime

--- databricks_cli/stack/api.py
+++ databricks_cli/stack/api.py
@@ -24,12 +24,8 @@
 import os
 import json
 from datetime import datetime
-<<<<<<< HEAD
 import time
 import copy
-=======
-import six
->>>>>>> Fixed timestamp error)
 
 import click
 
@@ -311,7 +307,6 @@ class StackApi(object):
         deploy_output = self.jobs_client.get_job(job_id)
         return physical_id, deploy_output
 
-<<<<<<< HEAD
     def _put_job(self, job_settings):
         """
         Given settings of the job in job_settings, create a new job. For purposes of idempotency
@@ -381,29 +376,6 @@ class StackApi(object):
         click.echo('Uploading {} from {} to Databricks workspace at {}'.format(object_type,
                                                                                local_path,
                                                                                workspace_path))
-=======
-    def deploy_workspace(self, resource_id, resource_properties, physical_id=None, overwrite=False):
-        click.echo("Deploying workspace asset %s with properties \n%s" % (resource_id, json.dumps(
-            resource_properties, indent=2, sort_keys=True, separators=(',', ': '))))
-        local_path = self.validate_source_path(resource_properties['source_path'])
-        workspace_path = resource_properties['workspace_path']
-
-        lang_fmt = WorkspaceLanguage.to_language_and_format(local_path)  # Guess language and format
-        language, fmt = None, None
-        if lang_fmt:
-            language, fmt = lang_fmt
-
-        if 'language' in resource_properties:
-            language = resource_properties['language']
-        if 'format' in resource_properties:
-            fmt = resource_properties['format']
-
-        object_type = "DIRECTORY" if os.path.isdir(local_path) else "NOTEBOOK"
-        if 'object_type' in resource_properties:
-            object_type = resource_properties['object_type']
-
-        click.echo('sync %s %s to %s' % (object_type, local_path, workspace_path))
->>>>>>> Updated documentation, cleaned up arguments and stack deploy/download code
         if object_type == 'NOTEBOOK':
             # Inference of notebook language and format
             language_fmt = WorkspaceLanguage.to_language_and_format(local_path)
@@ -418,7 +390,6 @@ class StackApi(object):
         elif object_type == 'DIRECTORY':
             self.workspace_client.import_workspace_dir(local_path, workspace_path, overwrite,
                                                        exclude_hidden_files=True)
-<<<<<<< HEAD
         else:
             # Shouldn't reach here because of verification of object_type above.
             assert False
@@ -542,149 +513,3 @@ class StackApi(object):
         """
         with open(path, 'w') as f:
             json.dump(data, f, indent=2, sort_keys=True)
-=======
-        if physical_id and workspace_path != physical_id['path']:
-            click.echo('Workspace asset %s had path changed from %s to %s' % (resource_id,
-                                                                              physical_id['path'],
-                                                                              workspace_path))
-        physical_id = {'path': workspace_path}
-        deploy_output = self.workspace_client.get_status_json(workspace_path)
-
-        return physical_id, deploy_output
-
-    def deploy_resource(self, resource, overwrite):
-        resource_id, resource_type, physical_id, deploy_output = None, None, None, None
-        try:
-            resource_id = resource[RESOURCE_ID]
-            resource_type = resource[RESOURCE_TYPE]
-            resource_properties = resource[RESOURCE_PROPERTIES]
-
-            # Deployment
-            physical_id = self.get_deployed_resource(resource_id, resource_type)
-
-            if resource_type == JOBS_TYPE:
-                physical_id, deploy_output = self.deploy_job(resource_id, resource_properties,
-                                                             physical_id)
-            elif resource_type == WORKSPACE_TYPE:
-                physical_id, deploy_output = self.deploy_workspace(resource_id,
-                                                                   resource_properties,
-                                                                   physical_id, overwrite)
-        except HTTPError as e:
-            if DEBUG_MODE:
-                traceback.print_tb(e.__traceback__)
-            click.echo(click.style('Error: %s' % e.response.json(), 'red'))
-        except KeyError as e:
-            click.echo('Error in config template: Missing %s, skipping resource' % e)
-        except Exception as e:
-            if DEBUG_MODE:
-                traceback.print_tb(e.__traceback__)
-            click.echo(e)
-
-        resource_deploy_info = {}
-        resource_deploy_info[RESOURCE_ID] = resource_id
-        resource_deploy_info[RESOURCE_TYPE] = resource_type
-        if six.py3:
-            resource_deploy_info['timestamp'] = datetime.now().timestamp()
-        else:
-            resource_deploy_info['timestamp'] = datetime.now().time()
-        resource_deploy_info['physical_id'] = physical_id
-        resource_deploy_info['deploy_output'] = deploy_output
-        return resource_deploy_info
-
-    def deploy(self, filename, overwrite, save_status_path):
-        parsed_conf = self.parse_config_file(filename)
-        stack_name = parsed_conf['name']
-        self.load_deploy_metadata(stack_name, save_status_path)
-
-        deploy_metadata = {}
-        deploy_metadata['name'] = stack_name
-        # deploy_metadata['version'] = parsed_conf['version']
-        # deploy_metadata['cli_version'] = CLI_VERSION
-        click.echo('Deploying stack %s' % stack_name)
-        deploy_metadata['resources'] = parsed_conf['resources']
-        deployed_resources = []
-        for resource in parsed_conf['resources']:
-            click.echo()
-            click.echo("Deploying resource")
-            deploy_status = self.deploy_resource(resource, overwrite)
-            if deploy_status:
-                deployed_resources.append(deploy_status)
-        deploy_metadata['deployed'] = deployed_resources
-        self.store_deploy_metadata(stack_name, deploy_metadata, save_status_path)
-
-    def download_workspace(self, resource_id, resource_properties, physical_id, overwrite):
-        click.echo("Downloading workspace asset %s with properties \n%s" % (resource_id, json.dumps(
-            resource_properties, indent=2, sort_keys=True, separators=(',', ': '))))
-        local_path = self.validate_source_path(resource_properties['source_path'])
-        workspace_path = resource_properties['workspace_path']
-
-        if 'format' in resource_properties:
-            fmt = resource_properties['format']
-        else:
-            fmt = WorkspaceFormat.SOURCE
-
-        object_type = "DIRECTORY" if os.path.isdir(local_path) else "NOTEBOOK"
-        if 'object_type' in resource_properties:
-            object_type = resource_properties['object_type']
-
-        click.echo('sync %s %s to %s' % (object_type, local_path, workspace_path))
-        if object_type == 'NOTEBOOK':
-            try:
-                self.workspace_client.export_workspace(workspace_path, local_path, fmt, overwrite)
-            except LocalFileExistsException:
-                click.echo('{} already exists locally as {}. Skip.'.format(workspace_path,
-                                                                           local_path))
-        elif object_type == 'DIRECTORY':
-            self.workspace_client.export_workspace_dir(workspace_path, local_path, overwrite)
-
-    def download_resource(self, resource, overwrite):
-        resource_id, resource_type, physical_id, deploy_output = None, None, None, None
-        try:
-            resource_id = resource[RESOURCE_ID]
-            resource_type = resource[RESOURCE_TYPE]
-            resource_properties = resource[RESOURCE_PROPERTIES]
-
-            # Deployment
-            physical_id = self.get_deployed_resource(resource_id, resource_type)
-            click.echo()
-            if resource_type == WORKSPACE_TYPE:
-                self.download_workspace(resource_id, resource_properties, physical_id, overwrite)
-        except HTTPError as e:
-            click.echo("HTTP Error: \n %s" % (json.dumps(e.response)))
-        except KeyError as e:
-            click.echo('Error in config template: Missing %s, skipping resource' % e)
-        except Exception as e:
-            if DEBUG_MODE:
-                traceback.print_tb(e.__traceback__)
-            click.echo(str(e))
-
-    def download(self, filename, overwrite):
-        parsed_conf = self.parse_config_file(filename)
-        # load_credentials(parsed_conf, ctx.obj['HC_CONTEXT'])
-        for resource in parsed_conf['resources']:
-            self.download_resource(resource, overwrite)
-
-    # WIP- describe stack
-    def describe(self, stack_name):
-        stored_deploy_metadata = self.load_deploy_metadata(stack_name)
-        click.echo(json.dump(stored_deploy_metadata, indent=2))
-        return stored_deploy_metadata
-
-    def get_job_info(self, deployed_jobs):
-        jobs = []
-        for job in deployed_jobs:
-            try:
-                job_info = self.jobs_client.get_job(job['job_id'])
-                job_url = '%s/#job/%s' % (self.api_client.host, str(job_info['job_id']))
-                jobs.append({'job_name': job['job_name'], 'job_url': job_url, 'job_info': job_info})
-            except HTTPError as e:
-                jobs.append({'job_name': job['job_name'], 'job_url': "Job doesn't exist",
-                             'job_info': e.response.json()})
-        return jobs
-
-    def get_workspace_info(self, workspace_paths):
-        workspace_infos = []
-        for workspace_path in workspace_paths:
-            workspace_infos += self.workspace_client.list_objects(workspace_path)
-        return workspace_infos
->>>>>>> Updated documentation, cleaned up arguments and stack deploy/download code
